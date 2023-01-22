90fc1ddf96e028e9c7e2da9d6cb5f9cc2447d22d
KshitijThareja <kshitijthareja03@gmail.com>
2023-01-22 23:03:14 +0530

    fix:added package in required

--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'blog/index.html')
+
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
 
 def post_new(request):
     if request.method == "POST":
